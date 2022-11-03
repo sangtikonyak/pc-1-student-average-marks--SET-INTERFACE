407509378813c19f0f0d64443a7c0e1ecd2ef117
Sangti konyak <ssangtikonyak@gmail.com>
2022-11-03 23:04:50 +0530

    commit 1

--- src/main/java/com/jap/Student.java
+++ src/main/java/com/jap/Student.java
@@ -8,9 +8,43 @@ public class Student {
 
     //create getter and setter for all the above attributes
 
-    //complete the parameterized constructor
-    public Student(String name, int rollNo, int totalMarks) {
-
+    public String getName() {
+        return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public int getTotalMarks() {
+        return totalMarks;
+    }
+
+    public void setTotalMarks(int totalMarks) {
+        this.totalMarks = totalMarks;
+    }
+
+    //complete the parameterized constructor
+    public Student(String name, int rollNo, int totalMarks) {
+       this.name=name;
+       this.rollNo=rollNo;
+       this.totalMarks=totalMarks;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", rollNo=" + rollNo +
+                ", totalMarks=" + totalMarks +
+                '}';
+    }
 }
--- src/main/java/com/jap/StudentService.java
+++ src/main/java/com/jap/StudentService.java
@@ -6,25 +6,36 @@ public class StudentService {
 
     public static Set<Student> getAllStudentData(){
         //create six student object by calling Student class parameterized constructor
+        Student[] student={new Student("sangti",12,100),new Student("khenou",11,120),
+        new Student("angjei",13,140),new Student("jaitok",14,150),
+        new Student("phongle",15,160)};
 
         //create HashSet object and add all the students object inside it
-
+        Set<Student> studentSet = new HashSet<>(Arrays.asList(student));
         //return the HashSet object
 
+        System.out.println("studentSet = " + studentSet);
+        System.out.println("studentSet.size() = " + studentSet.size());
 
-        return null;
+        return studentSet;
     }
 
 
     public static Set<String> getAllStudentNameInSorted(Set<Student> studentSet){
         //Create a TreeSet object
-
+        TreeSet<String> treeSet=new TreeSet<>();
         //Use Iterator object of type <Student> to iterate through the studentSet
-
+        Student student=null;
+        Iterator<Student> iterator=studentSet.iterator();
+        while (iterator.hasNext())
+        {
+            student=iterator.next();
+            treeSet.add(student.getName());
+        }
         //get all the name and add it inside the TreeSet object
-
-      //return the TreeSet object;
-        return null;
+        System.out.println("treeSet = " + treeSet);
+        //return the TreeSet object;
+        return treeSet;
     }
 
     public static Map<String,Integer> calculateAverage(Set<Student> studentSet){
@@ -39,7 +50,14 @@ public class StudentService {
         //add the student name and average marks in the HashMap object created in the
         //first line and return the map
 
-           return null;
+        Map<String,Integer> studentMap=new HashMap<>();
+        Integer average=0;
+        for (Student student : studentSet) {
+            average=student.getTotalMarks()/6;
+            studentMap.put(student.getName(),average);
+        }
+        System.out.println("studentMap = " + studentMap.entrySet());
+        return studentMap;
 
     }
 
@@ -47,6 +65,7 @@ public class StudentService {
         Set studentName = getAllStudentData();
         System.out.println(getAllStudentNameInSorted(studentName));
         System.out.println(calculateAverage(studentName));
+
     }
 
 
